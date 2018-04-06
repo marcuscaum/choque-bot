76e002401283956004b1c2eec9a304744d4639ad
marcuscaum <marcuslucas.jb@gmail.com>
2018-04-06 18:49:31 -0300

    Remove unnecessary command

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
 const enterChannel = message => {
   if (message.member.voiceChannel) {
     return message.member.voiceChannel.join();
